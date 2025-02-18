88d624c76b364c73ba24757b742c2e3367399371
mizael.santos <mizaelmiguel246@Gmail.com>
2025-02-18 09:41:19 -0300

    Adicionando metodo de subtração.

--- src/gitTeste/Main.java
+++ src/gitTeste/Main.java
@@ -1,6 +1,7 @@
 package gitTeste;
 
 import static gitTeste.Main.Calculadora.soma;
+import static gitTeste.Main.Calculadora.subtração;
 
 public class Main {
 
@@ -8,9 +9,13 @@ public class Main {
         public static int soma(int a, int b){
             return a+b;
         }
+    public static int subtração(int a, int b){
+            return a - b;
+    }
     }
 
     public static void main(String[] args) {
         System.out.println("Resultado:" + soma(5,3));
+        System.out.println("Resultado da subtração:" + subtração(5,3));
     }
 }
